997535fdb2a7cf64af7720671c246ede4791d1d1
42778 <anandakrishna.h@tataelxsi.co.in>
2025-07-29 14:00:08 +0530

    Made changes in button

--- Button.jsx
+++ Button.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 
 const Button = ({ label, onClick }) => {
   return (
-    <button onClick={onClick} className="px-4 py-2 bg-blue-500 text-white rounded">
-      {label}
     </button>
   );
 };
