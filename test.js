1f4d642c333544b258693c8b734769d267cbfada
42778 <anandakrishna.h@tataelxsi.co.in>
2025-09-08 12:25:36 +0530

    Made changes

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-console.log("hello");
-
-const prepare()=>{
-hello
-}
+// Log a greeting message to the console.
+// Changed the function name to logHello for better clarity.
+const logHello = () => {
+  console.log("hello");
+};
