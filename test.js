be6d0c8dcd49481599cd7f0aec773e8758f76570
42778 <anandakrishna.h@tataelxsi.co.in>
2025-09-18 15:56:15 +0530

    Made changes in button

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
 /**
  * Logs a greeting message to the console.
  */
-const greetUser = function logGreeting() {
-  console.log("hello");
+const greetUser = function logGreeting(name) {
+  if (!name) {
+    console.log("hello");
+  } else {
+    console.log("hello, " + name);
+  }
 };
