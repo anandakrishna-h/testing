1401f524bb30d9fac9c557327eaaaf4ad719b6d8
42778 <anandakrishna.h@tataelxsi.co.in>
2025-10-07 16:10:03 +0530

    Made changes

--- test.js
+++ test.js
@@ -2,6 +2,11 @@
  * Logs a greeting message to the console.
  */
 const greetUserByName = function(name) {
-  const greetingMessage = !name ? "hello" : "hello, " + name;
+  // Clean the name to remove any non-alphanumeric characters.
+  const nameCleaned = name ? name.replace(/[^a-zA-Z0-9\s]/g, '') : "";
+
+  // Determine the greeting message based on whether a name is provided.
+  const greetingMessage = nameCleaned ? "hello, " + nameCleaned : "hello";
+
   console.log(greetingMessage);
 };
